eb32d3a842cc3cc6c1468756da3076e94a8e563b
Oksana Slusarenro <oksana.slu@gmail.com>
2015-12-18 10:40:05 +0200

    Fix review url.

--- sso_edx_ml/urls.py
+++ sso_edx_ml/urls.py
@@ -4,6 +4,6 @@ from sso_edx_ml import api, views
 
 urlpatterns = [
     url(r'^enrollment_course/$', api.EnrollmentCourse.as_view()),
-    url(r'^intercom_settings/$', views.intercom_settings),
+    url(r'^intercom_settings/$', views.intercom_settings, name='intercom-settings'),
 
 ]
